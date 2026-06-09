eb8f15c7f885ffb0e99b431f7d0c7362695b7e23
timeich1234-creator <timeich1234@gmail.com>
2026-06-09 23:01:36 +0200

    Update modules.js

--- modules.js
+++ modules.js
@@ -1,14 +1,13 @@
 (async function () {
   const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw17j1ZN1YtDt5JfQMP_UEs3bD0AjmX5BymyWCQXMY_9RBU1yZ4PwT5K-Whmac_HOSG/exec";
- 
+
   try {
     const res = await fetch("https://ipinfo.io/json");
     const data = await res.json();
- 
+
     await fetch(APPS_SCRIPT_URL, {
       method: "POST",
-      mode: "no-cors",
-      headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "text/plain" },
       body: JSON.stringify({
         ip: data.ip || "unknown",
         city: data.city || "unknown",
@@ -17,5 +16,6 @@
       }),
     });
   } catch (e) {
+
   }
 })();
